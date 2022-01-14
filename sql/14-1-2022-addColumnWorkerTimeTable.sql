cd889eb1a460ec18d515cd4d9710463046d72fa3
Fulin.Leow <nonamime@gmail.com>
2022-01-14 17:20:52 +0800

    timesheet related file update

---
+++ sql/14-1-2022-addColumnWorkerTimeTable.sql
@@ -0,0 +1,24 @@
+ALTER TABLE
+    worker_time
+ADD
+    update_by TEXT;
+
+ALTER TABLE
+    worker_time
+ADD
+    update_at TEXT;
+
+ALTER TABLE
+    worker_time
+ADD
+    created_by TEXT;
+
+ALTER TABLE
+    worker_time
+ADD
+    created_at TEXT;
+
+ALTER TABLE
+    worker_time
+ADD
+    remark TEXT;
